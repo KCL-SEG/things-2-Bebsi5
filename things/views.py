fe42236e391582b8ffabbf41824caba6d787c082
Bebsi5 <rameez.ahmed@kcl.ac.uk>
2022-12-08 23:58:48 +0000

    final

--- things/views.py
+++ things/views.py
@@ -5,16 +5,5 @@ from .models import Thing
 
 def home(request):
     """Home view."""
-    submitted = False
-    if request.method == 'POST':
-        form = ThingForm(request.POST)
-        if form.is_valid():
-            form.save()
-    else:
-        form = ThingForm()
-        if 'submitted' in request.GET:
-            submitted = True
-
-    
-
-    return render(request, 'home.html', {'form': form, 'submitted': submitted})
+    form = ThingForm()
+    return render(request, 'home.html', {'form': form})

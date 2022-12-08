4491b44efaa4cae943265ea5c9978ae8dcc88aab
Bebsi5 <rameez.ahmed@kcl.ac.uk>
2022-12-08 23:51:54 +0000

    complete

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,15 @@
 """Forms of the project."""
 
-# Create your forms here.
+from django import forms
+from django.core.validators import RegexValidator
+from .models import *
+class ThingForm(forms.ModelForm):
+    """Form for the Thing model."""
+
+    class Meta:
+        """Meta class for the ThingForm."""
+
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+        description = forms.CharField(widget=forms.Textarea)
+        quantity = forms.IntegerField(widget=forms.NumberInput)
--- things/views.py
+++ things/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
+from .forms import ThingForm
+from .models import Thing
+
 
 def home(request):
-    return render(request, 'home.html', {'form': form})
+    """Home view."""
+    submitted = False
+    if request.method == 'POST':
+        form = ThingForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = ThingForm()
+        if 'submitted' in request.GET:
+            submitted = True
+
+    
+
+    return render(request, 'home.html', {'form': form, 'submitted': submitted})
